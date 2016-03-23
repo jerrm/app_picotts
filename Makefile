b128caa125153d58ae22e318114519bfd8315755
root <root@raspberrypi>
2016-03-23 13:13:36 -0400

    Initial update for Asterisk 13

--- Makefile
+++ Makefile
@@ -1,22 +1,31 @@
 #
 # Makefile for Asterisk PicoTTS application
 #
+# This program is free software, distributed under the terms of
+# the GNU General Public License Version 2. See the COPYING file
+# at the top of the source tree.
+
 
 INSTALL=install
-ASTLIBDIR=$(INSTALL_PREFIX)/usr/lib/asterisk
-MODULES_DIR=$(ASTLIBDIR)/modules
+ASTLIBDIR:=$(shell awk '/moddir/{print $$3}' /etc/asterisk/asterisk.conf)
+ifeq ($(strip $(ASTLIBDIR)),)
+        MODULES_DIR=$(INSTALL_PREFIX)/usr/lib/asterisk/modules
+else
+        MODULES_DIR=$(INSTALL_PREFIX)$(ASTLIBDIR)
+endif
 ASTETCDIR=$(INSTALL_PREFIX)/etc/asterisk
+SAMPLENAME=app_picotts.conf.sample
 
 CC=gcc
 OPTIMIZE=-O2
 DEBUG=-g
 
 LIBS+=
-CFLAGS+= -fPIC -Wall -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -D_REENTRANT  -D_GNU_SOURCE  
+CFLAGS+= -pipe -fPIC -Wall -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -D_REENTRANT  -D_GNU_SOURCE  
 
 all: _all
-	@echo " +-------- app_espeak Build Complete --------+"  
-	@echo " + app_picotts has successfully been built,   +"  
+	@echo " +-------- app_picotts Build Complete -------+"  
+	@echo " + app_picotts has successfully been built,  +"  
 	@echo " + and can be installed by running:          +"
 	@echo " +                                           +"
 	@echo " +               make install                +"  
@@ -48,24 +57,45 @@ install: _all
 	@echo " +              make samples                   +"
 	@echo " +---------------------------------------------+"
 
-samples:
-	mkdir -p $(DESTDIR)$(ASTETCDIR)
-	for x in *.sample; do \
-		if [ -f $(DESTDIR)$(ASTETCDIR)/`basename $$x .sample` ]; then \
-			if [ "$(OVERWRITE)" = "y" ]; then \
-				if cmp -s $(DESTDIR)$(ASTETCDIR)/`basename $$x .sample` $$x ; then \
-					echo "Config file $$x is unchanged"; \
-					continue; \
-				fi ; \
-				mv -f $(DESTDIR)$(ASTETCDIR)/`basename $$x .sample` $(DESTDIR)$(ASTETCDIR)/`basename $$x .sample`.old ; \
-			else \
-				echo "Skipping config file $$x"; \
-				continue; \
-			fi ;\
-		fi ; \
-		$(INSTALL) -m 644 $$x $(DESTDIR)$(ASTETCDIR)/`basename $$x .sample` ;\
-	done
+as_flite: _flite
+	@echo " +------ app_picotts Built as app_flite -------+"  
+	@echo " + app_picotts has successfully been built as  +"  
+	@echo " + app_flite.                                  +"
+	@echo " +                                             +"
+	@echo " + CAUTION:                                    +"
+	@echo " +                                             +"
+	@echo " + if installed it will overwrite and replace  +"
+	@echo " + app_flite                                   +"
+	@echo " +                                             +"
+	@echo " +---------------------------------------------+" 
+	@echo " + To replace app_flite, run:                  +"
+	@echo " +         make install_over_flite             +"  
+	@echo " +---------------------------------------------+" 
 
-ifneq ($(wildcard .*.d),)
-   include .*.d
-endif
+_flite: app_flite.so
+
+app_flite.o: app_picotts.c
+	$(CC) $(CFLAGS) -DAS_FLITE $(DEBUG) $(OPTIMIZE) -c -o app_flite.o app_picotts.c
+
+app_flite.so: app_flite.o
+	$(CC) -shared -Xlinker -x -o $@ $< $(LIBS)
+
+install_over_flite: _flite
+	$(INSTALL) -m 755 -d $(DESTDIR)$(MODULES_DIR)
+	$(INSTALL) -m 755 app_flite.so $(DESTDIR)$(MODULES_DIR)
+	
+	@echo " +---- app_picotts has replaced app_flite -----+"  
+	@echo " +                                             +"
+	@echo " + app_picotts has successfully been installed +"  
+	@echo " + over  app_flite                             +"  
+	@echo " +---------------------------------------------+"
+
+samples:
+	@mkdir -p $(DESTDIR)$(ASTETCDIR)
+	@if [ -f $(DESTDIR)$(ASTETCDIR)/$(CONFNAME) ]; then \
+		echo "Backing up previous config file as $(CONFNAME).old";\
+		mv -f $(DESTDIR)$(ASTETCDIR)/$(CONFNAME) $(DESTDIR)$(ASTETCDIR)/$(CONFNAME).old ; \
+	fi ;
+	$(INSTALL) -m 644 $(SAMPLENAME) $(DESTDIR)$(ASTETCDIR)/$(CONFNAME)
+	@echo " ------- app_picotts config Installed ---------"
+
--- app_picotts.c
+++ app_picotts.c
@@ -20,7 +20,7 @@
  *
  * \brief Say text to the user, using PicoTTS TTS engine.
  *
- * \author\verbatim Ramon Martinez <rampa@encomix.org>  based on works of Lefteris Zafiris <zaf.000@gmail.com>\endverbatim
+ * \author\verbatim Jerry Murdock based on works of Ramon Martinez <rampa@encomix.org> and Lefteris Zafiris <zaf.000@gmail.com>\endverbatim
  *
  * \extref PicoTTS text to speech Synthesis System
  *
@@ -44,15 +44,21 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision: 00 $")
 #include "asterisk/utils.h"
 #include "asterisk/strings.h"
 
+#ifdef AS_FLITE
+#define AST_MODULE "Flite"
+#else
 #define AST_MODULE "PicoTTS"
-#define FLITE_CONFIG "picotts.conf"
+#endif
+
+#define PICO_CONFIG "app_picotts.conf"
 #define MAXLEN 2048
 #define DEF_RATE 8000
-#define DEF_VOICE "es-ES"
+#define DEF_LANG "en-US"
 #define DEF_DIR "/tmp"
+#define DEF_VOLUME 1
 
 
-static char *app = "PicoTTS";
+static char *app = AST_MODULE;
 static char *synopsis = "Say text to the user, using PicoTTS TTS engine";
 static char *descrip =
   " PicoTTS(text[,intkeys][,language]): This will invoke the PicoTTS TTS engine, send a text string,\n"
@@ -61,12 +67,13 @@ static char *descrip =
 
 static int target_sample_rate;
 static int usecache;
+static double volume;
 static const char *cachedir;
-static const char *voice_name;
+static const char *voice_lang;
 static struct ast_config *cfg;
 static struct ast_flags config_flags = { 0 };
 
-static int read_config(void)
+static int read_config(const char *pico_conf)
 {
   const char *temp;
 
@@ -74,14 +81,15 @@ static int read_config(void)
   target_sample_rate = DEF_RATE;
   usecache = 0;
   cachedir = DEF_DIR;
-  voice_name = DEF_VOICE;
+  voice_lang = DEF_LANG;
+  volume = DEF_VOLUME;
 
-  cfg = ast_config_load(FLITE_CONFIG, config_flags);
+  cfg = ast_config_load(pico_conf, config_flags);
   if (!cfg || cfg == CONFIG_STATUS_FILEINVALID)
     {
       ast_log(LOG_WARNING,
               "PicoTTS: Unable to read config file %s. Using default settings\n",
-              FLITE_CONFIG);
+              PICO_CONFIG);
     }
   else
     {
@@ -91,12 +99,16 @@ static int read_config(void)
       if ((temp = ast_variable_retrieve(cfg, "general", "cachedir")))
         cachedir = temp;
 
-      if ((temp = ast_variable_retrieve(cfg, "general", "voice")))
-        voice_name = temp;
+      if ((temp = ast_variable_retrieve(cfg, "general", "language")))
+        voice_lang = temp;
 
       if ((temp = ast_variable_retrieve(cfg, "general", "samplerate")))
         target_sample_rate = atoi(temp);
+
+      if ((temp = ast_variable_retrieve(cfg, "general", "volume")))
+        volume = atof(temp);
     }
+
 
   if (target_sample_rate != 8000 && target_sample_rate != 16000)
     {
@@ -115,7 +127,6 @@ static int picotts_text_to_wave(const char *filedata, const char *language, cons
 
   sprintf(temp, "pico2wave -w %s -l %s '%s'", filedata, language, texttospeech);
 
-
   res_tts = system(temp);
   ast_log(LOG_WARNING, "PicoTTS: command %s, code %d.\n", temp, res_tts);
 
@@ -129,21 +140,19 @@ static int delete_wave(const char *filedata)
 }
 
 
-
-
 static int picotts_exec(struct ast_channel *chan, const char *data)
 {
   int res = 0;
   char *mydata;
   int writecache = 0;
   char MD5_name[33] = "";
-  int sample_rate;
   char cachefile[MAXLEN] = "";
-  char tmp_name[20];
-  char raw_tmp_name[26];
-  char rawpico_tmp_name[26];
-
+  char tmp_name[200];
+  char raw_tmp_name[200];
+  char rawpico_tmp_name[200];
   int voice;
+
+//  read_config(PICO_CONFIG);
 
   AST_DECLARE_APP_ARGS(args,
                        AST_APP_ARG(text);
@@ -153,7 +162,7 @@ static int picotts_exec(struct ast_channel *chan, const char *data)
 
   if (ast_strlen_zero(data))
     {
-      ast_log(LOG_ERROR, "PicoTTS requires t least an argument (text)\n");
+      ast_log(LOG_ERROR, "PicoTTS requires an argument (text)\n");
       return -1;
     }
 
@@ -166,24 +175,25 @@ static int picotts_exec(struct ast_channel *chan, const char *data)
 
 
   args.text = ast_strip_quoted(args.text, "\"", "\"");
-  args.lang = ast_strip_quoted(args.lang, "\"", "\"");
-  if (ast_strlen_zero(args.text))
-    {
+  if (ast_strlen_zero(args.text)) {
       ast_log(LOG_WARNING, "PicoTTS: No text passed for synthesis.\n");
       return res;
     }
 
-  if (ast_strlen_zero(args.lang))
-    {
-      ast_log(LOG_WARNING, "PicoTTS: language is default: %s.\n", voice_name);
+  if (args.lang)
+      ast_strip_quoted(args.lang, "\"", "\"");
+
+  if (ast_strlen_zero(args.lang))   {
+      ast_log(LOG_WARNING, "PicoTTS: language is default: %s.\n", voice_lang);
     }
   else
     {
-      voice_name = args.lang;
+      voice_lang = args.lang;
     }
 
   ast_debug(1, "PicoTTS:\nText passed: %s\nInterrupt key(s): %s\nVoice: %s\nRate: %d\n",
-            args.text, args.interrupt, voice_name, target_sample_rate);
+            args.text, args.interrupt, voice_lang, target_sample_rate);
+
 
   /*Cache mechanism */
   if (usecache)
@@ -201,13 +211,13 @@ static int picotts_exec(struct ast_channel *chan, const char *data)
           else
             {
               ast_debug(1, "PicoTTS: Cache file exists.\n");
-              if (chan->_state != AST_STATE_UP)
+              if (ast_channel_state(chan) != AST_STATE_UP)
                 ast_answer(chan);
-              res = ast_streamfile(chan, cachefile, chan->language);
+              res = ast_streamfile(chan, cachefile, ast_channel_language(chan));
               if (res)
                 {
                   ast_log(LOG_ERROR, "PicoTTS: ast_streamfile from cache failed on %s\n",
-                          chan->name);
+                           ast_channel_name(chan));
                 }
               else
                 {
@@ -221,44 +231,41 @@ static int picotts_exec(struct ast_channel *chan, const char *data)
 
   /* Create temp filenames */
   snprintf(tmp_name, sizeof(tmp_name), "/tmp/picotts_%li", ast_random() % 99999999);
+  snprintf(rawpico_tmp_name, sizeof(rawpico_tmp_name), "%s.wav", tmp_name);
   if (target_sample_rate == 8000)
-    snprintf(raw_tmp_name, sizeof(raw_tmp_name), "%s.wav", tmp_name);
+    snprintf(raw_tmp_name, sizeof(raw_tmp_name), "%s.sln", tmp_name);
   if (target_sample_rate == 16000)
-    {
-      snprintf(raw_tmp_name, sizeof(raw_tmp_name), "%s.gsm", tmp_name);
-      snprintf(rawpico_tmp_name, sizeof(raw_tmp_name), "%s.wav", tmp_name);
-    }
+    snprintf(raw_tmp_name, sizeof(raw_tmp_name), "%s.sln16", tmp_name);
 
   /* Invoke PicoTTS */
-  //picotts_init();
-  if (strcmp(voice_name, "en-US") == 0)
+  if (strcmp(voice_lang, "en-US") == 0)
     voice = 1;
-  else if (strcmp(voice_name, "en-GB") == 0)
+  else if (strcmp(voice_lang, "en-GB") == 0)
     voice = 2;
-  else if (strcmp(voice_name, "de-DE") == 0)
+  else if (strcmp(voice_lang, "de-DE") == 0)
     voice = 3;
-  else if (strcmp(voice_name, "es-ES") == 0)
+  else if (strcmp(voice_lang, "es-ES") == 0)
     voice = 4;
-  else if (strcmp(voice_name, "fr-FR") == 0)
+  else if (strcmp(voice_lang, "fr-FR") == 0)
     voice = 5;
-  else if (strcmp(voice_name, "it-IT") == 0)
+  else if (strcmp(voice_lang, "it-IT") == 0)
     voice = 6;
   else
     {
       ast_log(LOG_WARNING, "PicoTTS: Unsupported voice %s. Using default voice.\n",
-              voice_name);
+              voice_lang);
       voice = 1;
-      voice_name = "en-US";
+      voice_lang = DEF_LANG;
     }
 
-  res = picotts_text_to_wave(rawpico_tmp_name, voice_name, args.text);
+  res = picotts_text_to_wave(rawpico_tmp_name, voice_lang, args.text);
 
   char temp[1024];
-  sprintf(temp, "sox -v 0.8 %s -r 8000 -c1 %s resample -ql", rawpico_tmp_name, raw_tmp_name);
+  //sprintf(temp, "sox -v 0.8 %s -r 8000 -c1 %s resample -ql", rawpico_tmp_name, raw_tmp_name);
+  sprintf(temp, "sox -v %f  %s -q -r %d -c1 -t raw %s", volume, rawpico_tmp_name, target_sample_rate, raw_tmp_name);
+  ast_log(LOG_WARNING, "PicoTTS: sox command:  %s\n", temp);
   system(temp);
   unlink(rawpico_tmp_name);
-  sample_rate = 16000;
-
 
   if (res == -1)
     {
@@ -273,12 +280,12 @@ static int picotts_exec(struct ast_channel *chan, const char *data)
       ast_filecopy(tmp_name, cachefile, NULL);
     }
 
-  if (chan->_state != AST_STATE_UP)
+  if (ast_channel_state(chan) != AST_STATE_UP)
     ast_answer(chan);
-  res = ast_streamfile(chan, tmp_name, chan->language);
+  res = ast_streamfile(chan, tmp_name, ast_channel_language(chan));
   if (res)
     {
-      ast_log(LOG_ERROR, "PicoTTS: ast_streamfile failed on %s\n", chan->name);
+      ast_log(LOG_ERROR, "PicoTTS: ast_streamfile failed on %s\n", ast_channel_name(chan));
     }
   else
     {
@@ -290,10 +297,10 @@ static int picotts_exec(struct ast_channel *chan, const char *data)
   return res;
 }
 
-static int reload(void)
+static int reload_module(void)
 {
   ast_config_destroy(cfg);
-  read_config();
+  read_config(PICO_CONFIG);
   return 0;
 }
 
@@ -305,7 +312,7 @@ static int unload_module(void)
 
 static int load_module(void)
 {
-  read_config();
+  read_config(PICO_CONFIG);
   return ast_register_application(app, picotts_exec, synopsis, descrip) ?
          AST_MODULE_LOAD_DECLINE : AST_MODULE_LOAD_SUCCESS;
 }
@@ -313,6 +320,6 @@ static int load_module(void)
 AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "PicoTTS TTS Interface",
                 .load = load_module,
                 .unload = unload_module,
-                .reload = reload,
+                .reload = reload_module,
                 );
 
